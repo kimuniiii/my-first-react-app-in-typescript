47eecb25ac772de6cbb161b40b8d7caf2313b525
kimuniiii <kk.baseball.0824@gmail.com>
2020-09-19 22:38:53 +0900

    Function Componentに対するアノテーションのベストプラクティス

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+interface AppProps {
+  message? : string;
+}
+
+//「Function Component」に対するアノテーションのベストプラクティス
+const App: React.FunctionComponent<AppProps> = ({message}: AppProps) => {
+  return (
+  <div>
+    <h1>{message}</h1>
+    <h2>React Starter Kit in TypeScript</h2>
+  </div>
+  );
 };
+
+App.defaultProps = {
+  message : 'Hello, defaultProps',
+  // description : 'This is App Component'
+}
 
 export default App;
